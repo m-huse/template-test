cce3271b58cb37fbd3591338934dd0b62e711d36
m-huse <husem99@hotmail.no>
2025-07-12 12:01:36 +0200

    update: ferdig quote generator prosjekt

---
+++ quotes.js
@@ -0,0 +1,349 @@
+const localQuotes= [
+
+ {
+            "text": "Today is the tomorrow we worried about yesterday.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "It's easier to see the mistakes on someone else's paper.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Every man dies. Not every man really lives.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "The day is already blessed, find peace within it.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "From small beginnings come great things.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "It takes both sunshine and rain to make a rainbow.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "A beautiful thing is never perfect.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Every new day is another chance to change your life.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Some pursue happiness, others create it.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "We do what we do because we believe.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "The harder you fall, the higher you bounce.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Everyone smiles in the same language.",
+            "author": "Anonymousk",
+            "tag": ""
+        },
+        {
+            "text": "You can do what's reasonable or you can decide what's possible.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Friendship isn't a big thing. It's a million little things.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "A good plan today is better than a perfect plan tomorrow.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Someone remembers, someone cares; your name is whispered in someone's prayers.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Don't be pushed by your problems; be led by your dreams.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Change your thoughts, change your life!",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Today is the tomorrow you worried about yesterday.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "The day always looks brighter from behind a smile.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "A stumble may prevent a fall.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Put your future in good hands your own.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "What you see depends on what you're looking for.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Worry gives a small thing a big shadow.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "To get something you never had, you have to do something you never did.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Be thankful when you don't know something for it gives you the opportunity to learn.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Letting go is not the end of the world; it is the beginning of a new life.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Our greatest glory is not in never failing but rising everytime we fall.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Being right is highly overrated. Even a stopped clock is right twice a day.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Love is not blind; it simply enables one to see things others fail to see.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "If you get up one more time than you fall, you will make it through.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Don't focus on making the right decision, focus on making the decision the right one.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Love is just a word until someone comes along and gives it meaning.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "We all have problems. The way we solve them is what makes us different.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Invent your world. Surround yourself with people, color, sounds, and work that nourish you.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Every day may not be good, but there's something good in every day.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Some people think it's holding that makes one strong sometimes it's letting go.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Why worry about things you cannot control when you can keep yourself busy controlling the things that depend on you?",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "It is better to take many small steps in the right direction than to make a great leap forward only to stumble backward.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "As the rest of the world is walking out the door, your best friends are the ones walking in.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Why compare yourself with others? No one in the entire world can do a better job of being you than you.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "A good teacher is like a candle it consumes itself to light the way for others.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Life is not measured by the breaths we take, but by the moments that take our breath.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "The real measure of your wealth is how much youd be worth if you lost all your money.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Though no one can go back and make a brand new start, anyone can start from now and make a brand new ending.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "A friend is someone who understands your past, believes in your future, and accepts you just the way you are.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Giving up doesn't always mean you are weak. Sometimes it means that you are strong enough to let go.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Never miss an opportunity to make others happy, even if you have to leave them alone in order to do it.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Yesterday is history. Tomorrow is a mystery. And today? Today is a gift that's why they call it the present.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "When you don't know what you believe, everything becomes an argument. Everything is debatable. But when you stand for something, decisions are obvious.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Every sixty seconds you spend angry, upset or mad, is a full minute of happiness you will never get back.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "If we are facing in the right direction, all we have to do is keep on walking.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Peace of mind is not the absence of conflict from life, but the ability to cope with it.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "An obstacle may be either a stepping stone or a stumbling block.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "I've never seen a smiling face that was not beautiful.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Kindness is the greatest wisdom.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Don't miss all the beautiful colors of the rainbow looking for that pot of gold.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "You don't drown by falling in water. You drown by staying there.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Never be afraid to try, remember... Amateurs built the ark, Professionals built the Titanic.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "A smile is a light in the window of your face to show your heart is at home.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "You may only be someone in the world, but to someone else, you may be the world.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "A bend in the road is not the end of the road...unless you fail to make the turn.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "One who asks a question is a fool for five minutes; one who does not ask a question remains a fool forever.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "Courage is the discovery that you may not win, and trying when you know you can lose.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "A good rest is half the work.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "All the flowers of all the tomorrows are in the seeds of today.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "A man is not where he lives but where he loves.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "The world does not happen to you it happens from you.",
+            "author": "Anonymous",
+            "tag": "general"
+        },
+        {
+            "text": "More powerful than the will to win is the courage to begin.",
+            "author": "Anonymous",
+            "tag": "general"
+        }
+
+    ];
